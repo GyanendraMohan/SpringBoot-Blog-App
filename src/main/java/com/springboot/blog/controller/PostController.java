a793e17e02c63f44b2fc231d5e505d91ad2f0577
GyanendraMohan <gmpatel152@gmail.com>
2023-10-09 13:05:50 +0530

    created post blog api

---
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -0,0 +1,30 @@
+package com.springboot.blog.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.service.PostService;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+    
+    private PostService postService; //injecting interface not  the class hence we are making loose coupling with classes.
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    //creating blog post API
+
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+}
